cc9dbeb2d8732fb24f9cf386e3c3ce1fa8f7cab4
Lance <2418640628@qq.com>
2026-03-31 09:21:48 +0800

    Add SudokuCLI and implement Observer pattern to demonstrate model-view decoupling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // use cil to view the logic support by model
+        SudokuModel model = new SudokuModel();
+        SudokuCLI cli = new SudokuCLI();
+        model.addObserver(cli);
+
+        System.out.println("Game Started!");
+
+
+        System.out.println("\nAction: Setting value 5 at (0,0)");
+        model.setValue(0, 0, 5);
+
+        // undo
+        System.out.println("\nAction: Performing Undo");
+        model.undo();
+
+       // hint
+        System.out.println("\nAction: Getting Hint");
+        model.giveHint();
     }
 }
---
+++ src/main/java/org/example/SudokuCLI.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Observable;
+import java.util.Observer;
+
+// Simple CLI view to demonstrate Observer pattern and model-view decoupling
+public class SudokuCLI implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+        SudokuModel model = (SudokuModel) o;
+        displayBoard(model);
+    }
+
+    private void displayBoard(SudokuModel model) {
+        System.out.println("\n--- Current Sudoku Board ---");
+        for (int r = 0; r < SudokuModel.SIZE; r++) {
+            if (r % 3 == 0 && r != 0) System.out.println("---------------------");
+            for (int c = 0; c < SudokuModel.SIZE; c++) {
+                if (c % 3 == 0 && c != 0) System.out.print("| ");
+                int val = model.getValue(r, c);
+                System.out.print((val == 0 ? "." : val) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
