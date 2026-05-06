c67e93ed2e2c2392482f3c9005e6c26fa38e9dfb
Lance <2418640628@qq.com>
2026-05-06 18:13:36 +0800

    Implement full interface-based decoupling for MVC and CLI

--- src/main/java/org/example/SudokuCLI.java
+++ src/main/java/org/example/SudokuCLI.java
@@ -8,15 +8,17 @@ public class SudokuCLI implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
-        SudokuModel model = (SudokuModel) o;
-        displayBoard(model);
+        if (o instanceof ISudokuModel) {
+            ISudokuModel model = (ISudokuModel) o;
+            displayBoard(model);
+        }
     }
 
-    private void displayBoard(SudokuModel model) {
+    private void displayBoard(ISudokuModel model) {
         System.out.println("\n--- Current Sudoku Board ---");
-        for (int r = 0; r < SudokuModel.SIZE; r++) {
+        for (int r = 0; r < 9; r++) {
             if (r % 3 == 0 && r != 0) System.out.println("---------------------");
-            for (int c = 0; c < SudokuModel.SIZE; c++) {
+            for (int c = 0; c < 9; c++) {
                 if (c % 3 == 0 && c != 0) System.out.print("| ");
                 int val = model.getValue(r, c);
                 System.out.print((val == 0 ? "." : val) + " ");
